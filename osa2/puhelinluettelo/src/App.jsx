66154d2210a60be6c50e208523ab04ee478c00cf
pr0fix <bgx152@myy.haaga-helia.fi>
2024-02-14 20:24:47 +0200

    osa2 tehtava 2.6

---
+++ osa2/puhelinluettelo/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  addPerson();
+};
+
+const addPerson = () => {
+  setPersons([...persons, {name: newName}]);
+  setNewName("");
+};
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input type='text' value={newName} onChange={(e) => setNewName(e.target.value)}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons && persons.map(person => <p key={person.name}>{person.name}</p>)}
+    </div>
+  )
+
+}
+
+export default App
