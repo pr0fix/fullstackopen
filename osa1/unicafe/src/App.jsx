90ae0a4129c3f2180603f679a6d7ed537c247422
pr0fix <bgx152@myy.haaga-helia.fi>
2024-02-14 10:16:41 +0200

    osa1 tehtava 1.10

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
     <>
@@ -11,15 +11,12 @@ const App = () => {
 		<Button handleClick={() => setGood(good+1)}>good</Button>
 		<Button handleClick={() => setNeutral(neutral+1)}>neutral</Button>
 		<Button handleClick={() => setBad(bad+1)}>bad</Button>
-		<h2>statistics</h2>
 		<Statistics good={good} neutral={neutral} bad={bad}/>
 	</>
-
-  )
+  );
 }
 
 const Button = (props) => {
-
 	return(
 		<button onClick={props.handleClick}>{props.children}</button>
 	);
@@ -30,24 +27,32 @@ const Statistics = (props) => {
 	const total = props.good + props.neutral + props.bad;
 	const average = (props.good - props.bad) / total || 0;
 	const positive = (props.good/total) * 100 || 0;
-	
-	
+
 	return(
 		<>
-		{props.good > 0 || props.neutral > 0 || props.bad > 0 ?
-		<>
-		<p>good {props.good}</p>
-		<p>neutral {props.neutral}</p>
-		<p>bad {props.bad}</p>
-		<p>all {total}</p>
-		<p>average {average}</p>
-		<p>positive {positive} %</p></>
-		: "No feedback given"}
+		<h2>statistics</h2>
+			{props.good > 0 || props.neutral > 0 || props.bad > 0 ?
+				<>
+				<StatisticLine text="good" value={props.good}/>
+				<StatisticLine text="neutral" value={props.neutral}/>
+				<StatisticLine text="bad" value={props.bad}/>
+				<StatisticLine text="all" value={total}/>
+				<StatisticLine text="average" value={average}/>
+				<StatisticLine text="positive" value={positive}/>
+				</>
+			: "No feedback given"}
 		</>
 	
 	);
 }
 
+const StatisticLine = (props) => {
+	return(
+	<>
+		<p>{props.text} {props.value}</p>
+	</>
+	);
+}
 
 
 export default App
