53d2512fc631f6ac34a91fab2df63cc831bfae9f
pr0fix <bgx152@myy.haaga-helia.fi>
2024-02-14 10:27:15 +0200

    osa 1 tehtava 1.11

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -29,27 +29,31 @@ const Statistics = (props) => {
 	const positive = (props.good/total) * 100 || 0;
 
 	return(
-		<>
-		<h2>statistics</h2>
-			{props.good > 0 || props.neutral > 0 || props.bad > 0 ?
-				<>
-				<StatisticLine text="good" value={props.good}/>
-				<StatisticLine text="neutral" value={props.neutral}/>
-				<StatisticLine text="bad" value={props.bad}/>
-				<StatisticLine text="all" value={total}/>
-				<StatisticLine text="average" value={average}/>
-				<StatisticLine text="positive" value={positive}/>
-				</>
-			: "No feedback given"}
-		</>
-	
+	<>
+	<h2>statistics</h2>
+	{props.good > 0 || props.neutral > 0 ||props.bad > 0?
+	<table>
+		<tbody>
+			<StatisticLine text="good" value={props.good}/>
+			<StatisticLine text="neutral" value={props.neutral}/>
+	 		<StatisticLine text="bad" value={props.bad}/>
+	 		<StatisticLine text="all" value={total}/>
+	 		<StatisticLine text="average" value={average}/>
+			<StatisticLine text="positive" value={`${positive} %`} />
+		</tbody>
+	</table>
+	: "No feedback given"}
+	</>
 	);
 }
 
 const StatisticLine = (props) => {
 	return(
 	<>
-		<p>{props.text} {props.value}</p>
+	<tr>
+		<td>{props.text}</td>
+		<td>{props.value}</td>
+	</tr>
 	</>
 	);
 }
