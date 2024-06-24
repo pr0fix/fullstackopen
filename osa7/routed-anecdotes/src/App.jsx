57699fa8b894ccf5b0b87c62c4af0a8da828b2da
pr0fix <bgx152@myy.haaga-helia.fi>
2024-06-24 22:48:27 +0300

    osa7 tehtava 7.6

--- osa7/routed-anecdotes/src/App.jsx
+++ osa7/routed-anecdotes/src/App.jsx
@@ -82,9 +82,9 @@ const Footer = () => (
 );
 
 const CreateNew = (props) => {
-  const content = useField("text");
-  const author = useField("text");
-  const info = useField("text");
+  const { reset: resetContent, ...content } = useField("text");
+  const { reset: resetAuthor, ...author } = useField("text");
+  const { reset: resetInfo, ...info } = useField("text");
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
@@ -99,9 +99,7 @@ const CreateNew = (props) => {
   };
 
   const handleReset = () => {
-    content.reset();
-    author.reset();
-    info.reset();
+    resetContent(), resetAuthor(), resetInfo();
   };
 
   return (
