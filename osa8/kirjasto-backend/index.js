4225ca9b8f02d7a3bf8f56a697aff8becf766458
pr0fix <bgx152@myy.haaga-helia.fi>
2024-08-23 18:17:18 +0300

    osa8 tehtava 8.4

--- osa8/kirjasto-backend/index.js
+++ osa8/kirjasto-backend/index.js
@@ -114,7 +114,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -123,7 +123,14 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (parent, args) => {
+      const { author } = args;
+
+      if (author) {
+        return books.filter((book) => book.author === author);
+      }
+      return books;
+    },
     allAuthors: () => authors,
   },
   Author: {
