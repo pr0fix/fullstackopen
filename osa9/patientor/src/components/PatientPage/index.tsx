b8db75cf09b5dbde63a9cc60dd3d3740a40de016
pr0fix <bgx152@myy.haaga-helia.fi>
2024-09-20 23:06:39 +0300

    osa9 tehtava 9.24

--- osa9/patientor/src/components/PatientPage/index.tsx
+++ osa9/patientor/src/components/PatientPage/index.tsx
@@ -2,7 +2,14 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Gender, Patient } from "../../types";
 import patients from "../../services/patients";
-import { Box, Typography } from "@mui/material";
+import {
+  Alert,
+  Box,
+  CircularProgress,
+  List,
+  ListItem,
+  Typography,
+} from "@mui/material";
 import MaleIcon from "@mui/icons-material/Male";
 import FemaleIcon from "@mui/icons-material/Female";
 import TransgenderIcon from "@mui/icons-material/Transgender";
@@ -46,20 +53,41 @@ const PatientPage: React.FC = () => {
     fetchPatientInfo();
   }, [id]);
 
-  if (loading) return <div>loading...</div>;
+  if (loading) return <CircularProgress />;
 
-  if (error) return <div>{error}</div>;
+  if (error) return <Alert severity="error">{error}</Alert>;
 
-  if (!patient) return <div>No patient data available</div>;
+  if (!patient) return <Typography>No patient data available</Typography>;
 
   return (
-    <Box sx={{ marginTop: 2 }}>
-      <Typography sx={{ fontWeight: "bold" }} variant="h5">
-        {patient.name} {getGenderIcon(patient.gender)}
-      </Typography>
+    <Box>
       <Box sx={{ marginTop: 2 }}>
-        <Typography>ssn: {patient.ssn}</Typography>
-        <Typography>occupation: {patient.occupation}</Typography>
+        <Typography sx={{ fontWeight: "bold" }} variant="h5">
+          {patient.name} {getGenderIcon(patient.gender)}
+        </Typography>
+        <Box sx={{ marginTop: 2 }}>
+          <Typography>ssn: {patient.ssn}</Typography>
+          <Typography>occupation: {patient.occupation}</Typography>
+        </Box>
+      </Box>
+      <Box sx={{ marginTop: 2 }}>
+        <Typography sx={{ fontWeight: "bold" }} variant="h6">
+          entries
+        </Typography>
+        {patient.entries.map((e) => (
+          <Box key={e.id}>
+            <Typography sx={{ fontStyle: "italic" }}>
+              {e.date} {e.description}
+            </Typography>
+            <List sx={{ listStyleType: "disc", marginLeft: 5, marginTop: 1 }}>
+              {e.diagnosisCodes?.map((code, idx) => (
+                <ListItem sx={{ display: "list-item", padding: 0 }} key={idx}>
+                  <Typography>{code}</Typography>
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+        ))}
       </Box>
     </Box>
   );
